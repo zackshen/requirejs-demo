0fdaee869f9009d32fea16f4dea322a64d9fcaf9
zack <zackshen0526@gmail.com>
2015-05-09 14:22:48 +0800

    delete

--- js/module.js
+++
@@ -1,5 +0,0 @@
-define("crossflowui", function(require, exports, module) {
-    //exports.modulea = require('./module_a');
-    //exports.moduleb = require('./module_b');
-    require('./module_a')
-});
